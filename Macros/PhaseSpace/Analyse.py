c76645f8bdfcb09078edcea3f91b627419bb1569
Samuel Paul Grant <Samuel Grant>
2024-03-01 13:29:40 -0600

    Updates to /PhaseSpace.

--- Macros/PhaseSpace/Analyse.py
+++ Macros/PhaseSpace/Analyse.py
@@ -7,6 +7,7 @@ Analyse the phase space at the end of M5
 # External libraries
 import numpy as np
 import pandas as pd
+import math
 
 # Internal libraries 
 import Utils as ut
@@ -15,13 +16,32 @@ import Utils as ut
 # Run
 # --------------------
 
-def Run():
+def RunOffsetScan():
     
     # Get configs and their aliases
     configs_ = { 
-        "NoWedge" : "No wedge"
-        ,"WedgeMinus5mm" : "$\minus5$ mm"
-        ,"WedgePlus5mm" : "$\plus5$ mm"
+        "NoWedge": "No wedge",
+        "WedgeMinus10mm": "$-10$ mm",
+        "WedgeMinus9mm": "$-9$ mm",
+        "WedgeMinus8mm": "$-8$ mm",
+        "WedgeMinus7mm": "$-7$ mm",
+        "WedgeMinus6mm": "$-6$ mm",
+        "WedgeMinus5mm": "$-5$ mm",
+        "WedgeMinus4mm": "$-4$ mm",
+        "WedgeMinus3mm": "$-3$ mm",
+        "WedgeMinus2mm": "$-2$ mm",
+        "WedgeMinus1mm": "$-1$ mm",
+        "Wedge0mm": "$0$ mm",
+        "WedgePlus1mm": "$+1$ mm",
+        "WedgePlus2mm": "$+2$ mm",
+        "WedgePlus3mm": "$+3$ mm",
+        "WedgePlus4mm": "$+4$ mm",
+        "WedgePlus5mm": "$+5$ mm",
+        "WedgePlus6mm": "$+6$ mm",
+        "WedgePlus7mm": "$+7$ mm",
+        "WedgePlus8mm": "$+8$ mm",
+        "WedgePlus9mm": "$+9$ mm",
+        "WedgePlus10mm": "$+10$ mm"
     }
 
     # Define column names
@@ -29,7 +49,12 @@ def Run():
 
     # Histogram holder, keys are the aliases (labels)
     hists_ = {alias: [] for alias in configs_.values()}
+    histsMasked_ = {alias: [] for alias in configs_.values()}
 
+    # Graph lists 
+    offsets_ = []
+    normEntries_ = []
+    normEntriesError_ = []
 
     for config, alias in configs_.items():
 
@@ -37,26 +62,109 @@ def Run():
 
         print(f"---> Analysing {finName}")
 
+        # --------------------
         # Get data
+        # --------------------
         data = pd.read_csv(finName, delim_whitespace=True, header=None, names=columns_)
 
+        # --------------------
+        # Fill histogram data
+        # --------------------
         # Plot single distribution
         ut.Plot1D(data=data["pz"], nbins=28, xmin=-0.07, xmax=0.07, title=alias, xlabel="$\Delta p / p_{0}$", ylabel="Muons / 0.005", fout=f"../../Images/{config}/h1_muons_end_pz_{config}.png") 
-
         # Append to list
-        hists_[alias].append(data["pz"])
+        hists_[alias] = data["pz"]
+        mask = (data["pz"] <= 0.002) & (data["pz"] >= -0.002)
+        histsMasked_[alias] = data["pz"][mask]
+
+        # --------------------
+        # Fill graph data
+        # --------------------
+        if alias == "No wedge": continue
+        # Convert the alias to a number
+        offset = int(alias.split(" ")[0].strip("$").replace("+", ""))
+        # Append offset
+        offsets_.append(offset)
+        # Append normalised entries with %2 of magic momentum.
+        normEntries = len(histsMasked_[alias]) / len(histsMasked_["No wedge"])
+        normEntriesError = normEntries * math.sqrt( (math.sqrt(len(histsMasked_[alias]))/len(histsMasked_[alias]))**2 + (math.sqrt(len(histsMasked_["No wedge"]))/len(histsMasked_["No wedge"]))**2 ) 
+        normEntries_.append(normEntries) 
+        normEntriesError_.append(normEntriesError)
 
     # Overlay the histograms
-    ut.Plot1DOverlay(hists_, nbins=28, xmin=-0.07, xmax=0.07, xlabel="$\Delta p / p_{0}$", ylabel="Muons / 0.005", fout=f"../../Images/h1_muons_end_pz_overlay.png", includeBlack=True) 
-        
+    # Just a selection, the graph is too messy otherwise
+    selectedHists_ = {}
+    for alias, hist_data in hists_.items():
+        if alias in ["No wedge", "$-10$ mm", "$-5$ mm", "$0$ mm", "$+5$ mm", "$+10$ mm"]: # "WedgeMinus10mm", "WedgeMinus5mm", "Wedge0mm", "WedgePlus5mm", "WedgePlus10mm"]:
+            selectedHists_[alias] = hist_data
+
+    selectedHistsMasked_ = {}
+    for alias, hist_data in histsMasked_.items():
+        if alias in ["No wedge", "$-10$ mm", "$-5$ mm", "$0$ mm", "$+5$ mm", "$+10$ mm"]: # "WedgeMinus10mm", "WedgeMinus5mm", "Wedge0mm", "WedgePlus5mm", "WedgePlus10mm"]:
+            selectedHistsMasked_[alias] = hist_data
+
+    ut.Plot1DOverlay(selectedHists_, nbins=70, xmin=-0.07, xmax=0.07, xlabel="$\Delta p / p_{0}$", ylabel="$\mu^{+}$ / 0.002", fout=f"../../Images/h1_muons_end_pz_overlay.png", includeBlack=True, colours_extended=False) 
+    ut.Plot1DOverlay(selectedHistsMasked_, nbins=70, xmin=-0.07, xmax=0.07, title=r"$|\Delta p / p_{0}| \leq 0.2\%$", xlabel="$\Delta p / p_{0}$", ylabel="$\mu^{+}$ / 0.002", fout=f"../../Images/h1_muons_end_pz_overlay_pm0.002.png", includeBlack=True, colours_extended=False) 
+
+    # Plot the 'ctags' 
+    ut.PlotNormEntriesGraph(x=offsets_, y=normEntries_, yerr=normEntriesError_, xlabel="Wedge offset [mm]", ylabel=r"$\mu^{+} (|\Delta p / p_{0}| \leq 0.2\%)$ [normalised]", fout="../../Images/gr_norm_entries_pm0.002.png")
+
     return
 
+
+def RunWedgeCooling(config="WedgeMinus7mmBeforeAfter"):
+
+    # --------------------
+    # Get data
+    # --------------------
+    # File names
+    finNameBefore = f"../../Data/{config}/muon_all_before_wedge.dat"
+    finNameAfter = f"../../Data/{config}/muon_all_after_wedge.dat"
+    print(f"---> Analysing {finNameBefore} and {finNameAfter}")
+    # Define column names
+    columns_ = ["PID", "s", "x", "px", "y", "py", "z", "pz", "ele", "sx", "sy", "sz"]  
+    # DataFrames
+    dataBefore = pd.read_csv(finNameBefore, delim_whitespace=True, header=None, names=columns_)
+    dataAfter = pd.read_csv(finNameAfter, delim_whitespace=True, header=None, names=columns_)
+
+    # mask = dataBefore["pz"] > 0.02
+    # dataBefore = dataBefore[mask]
+
+    # Merge 
+    # These should be the same muons before and after
+    dataWedge = dataBefore.merge(dataAfter, on=["PID"], suffixes=("_before", "_after"), how="inner")
+
+    print("\nBefore, After, Common")
+    print(len(dataBefore),",",len(dataAfter),",",len(dataWedge))
+    print()
+
+    # Plot 1D momentum distributions for sanity
+    # Not sure why we have such low stats
+    # are these the ones that actually intersect with the wedge? Surely not? 
+    # Is it because I exclude the lost or decayed ones? 
+    ut.Plot1D(data=dataWedge["pz_before"], nbins=28, xmin=-0.07, xmax=0.07, title="Before", xlabel="$\Delta p / p_{0}$", ylabel="Muons / 0.005", fout=f"../../Images/{config}/h1_pz_muons_before.png") 
+    ut.Plot1D(data=dataWedge["pz_after"], nbins=28, xmin=-0.07, xmax=0.07, title="After", xlabel="$\Delta p / p_{0}$", ylabel="Muons / 0.005", fout=f"../../Images/{config}/h1_pz_muons_after.png") 
+    hists_ = { 
+        "Before" : dataWedge["pz_before"]
+        ,"After" : dataWedge["pz_after"]
+    }
+    # ut.Plot1DOverlay(hists_, nbins=70, xmin=-0.07, xmax=0.07, title=r"0 mm offset, $|\Delta p / p_{0}| \leq 0.2\%$", xlabel="$\Delta p / p_{0}$", ylabel="$\mu^{+}$ / 0.002", fout=f"../../Images/h1_muons_before_after.png", includeBlack=False, colours_extended=False) 
+    # ut.Plot1DOverlay(hists_, nbins=70, xmin=-0.07, xmax=0.07, title="0 mm offset", xlabel="$\Delta p / p_{0}$", ylabel="$\mu^{+}$ / 0.002", fout=f"../../Images/h1_muons_before_after.png", includeBlack=False, colours_extended=False) 
+    ut.Plot1DOverlay(hists_, nbins=28, xmin=-0.07, xmax=0.07, title="$-7$ mm offset", xlabel="$\Delta p / p_{0}$", ylabel="$\mu^{+}$ / 0.002", fout=f"../../Images/h1_muons_before_after.png", includeBlack=False, colours_extended=False) 
+
+    ut.Plot2D(x=dataWedge["pz_before"], y=dataWedge["pz_after"], nBinsX=28, xmin=-0.07, xmax=0.07, nBinsY=28, ymin=-0.07, ymax=0.07, title="0 mm offset", xlabel=r"$\Delta p / p_{0}$ (before wedge)", ylabel=r"$\Delta p / p_{0}$ (after wedge)", fout=f"../../Images/{config}/h2_pz_muons_before_after.png") # , log=True) 
+    ut.Plot2DWith1DProj(x=dataWedge["pz_before"], y=dataWedge["pz_after"], nBinsX=28, xmin=-0.07, xmax=0.07, nBinsY=28, ymin=-0.07, ymax=0.07, title="0 mm offset", xlabel=r"$\Delta p / p_{0}$ (before wedge)", ylabel=r"$\Delta p / p_{0}$ (after wedge)", fout=f"../../Images/{config}/h2_proj_pz_muons_before_after.png" , logZ=False) 
+
+    return
 # --------------------
 # Main
 # --------------------
 
 def main():
-    Run() 
+
+    # RunOffsetScan() 
+
+    RunWedgeCooling() 
 
 if __name__ == "__main__":
     main()
--- Macros/PhaseSpace/Utils.py
+++ Macros/PhaseSpace/Utils.py
@@ -31,6 +31,28 @@ def Round(value, sf):
         return truncated_str
 
 import numpy as np
+
+def PadShorterArray(x, y):
+    # Pad the shorter array with zeros to match the length of the longer array.
+    max_length = max(len(x), len(y))
+    
+    # Pad shorter array with zeros to match the length of the longer array
+    x_padded = np.pad(x, (0, max_length - len(x)), mode='constant')
+    y_padded = np.pad(y, (0, max_length - len(y)), mode='constant')
+    
+    return x_padded, y_padded
+
+def TruncateLongerArray(x, y):
+    # Truncate the longer array to match the length of the shorter array.
+
+    min_length = min(len(x), len(y))
+    
+    # Truncate longer array to match the length of the shorter array
+    x_truncated = x[:min_length]
+    y_truncated = y[:min_length]
+    
+    return x_truncated, y_truncated
+
 from scipy import stats
 
 # Stats for histograms tends to assume a normal distribution
@@ -65,6 +87,13 @@ colours = [
     (0.4980392156862745, 0.4980392156862745, 0.4980392156862745),   # Gray 
     (0.7372549019607844, 0.7411764705882353, 0.13333333333333333)  # Yellow
 ]
+
+def GetExtendedColours(N, gradient='viridis'):
+    # Generate a color map gradient starting from black
+    cmap = plt.cm.get_cmap(gradient, N)
+    # Extract the RGB values from the color map
+    colours_extended = [cmap(i)[:3] for i in range(N)]
+    return colours_extended
 
 def Plot1D(data, nbins=100, xmin=-1.0, xmax=1.0, title=None, xlabel=None, ylabel=None, fout="hist.png", legPos="best", stats=True, peak=False, underOver=False, errors=False, NDPI=300):
     
@@ -114,10 +143,13 @@ def Plot1D(data, nbins=100, xmin=-1.0, xmax=1.0, title=None, xlabel=None, ylabel
 
 from matplotlib.ticker import ScalarFormatter
 
-def Plot1DOverlay(data_dict, nbins=100, xmin=-1.0, xmax=1.0, title=None, xlabel=None, ylabel=None, fout="hist.png", legPos="best", NDPI=300, includeBlack=False, logY=False, legFontSize=12):
+def Plot1DOverlay(data_dict, nbins=100, xmin=-1.0, xmax=1.0, title=None, xlabel=None, ylabel=None, fout="hist.png", legPos="best", NDPI=300, includeBlack=False, logY=False, legFontSize=12, colours_extended=False):
 
     # Create figure and axes
     fig, ax = plt.subplots()
+
+    # colours = colours
+    # if colours_extended: colours = GetExtendedColours(len(data_dict))
 
     # Iterate over the hists_dict and plot each one
     for i, (label, hist) in enumerate(data_dict.items()):
@@ -156,52 +188,232 @@ def Plot1DOverlay(data_dict, nbins=100, xmin=-1.0, xmax=1.0, title=None, xlabel=
     # Clear memory
     plt.close()
 
-# Example usage:
-# hists_dict = {
-#     'Label1': data1,
-#     'Label2': data2,
-#     ...
-# }
-# Plot1DOverlay(hists_dict, title='Histogram Overlay', xlabel='X Label', ylabel='Y Label', fout='output.png')
+def PlotGraph(x, y, xerr=[], yerr=[], title=None, xlabel=None, ylabel=None, fout="scatter.png", NDPI=300):
 
+   # Create a scatter plot with error bars using NumPy arrays 
 
-# def Plot1DOverlay(hists, nbins=100, xmin=-1.0, xmax=1.0, title=None, xlabel=None, ylabel=None, fout="hist.png", labels=None, legPos="upper right", NDPI=300, includeBlack=False, logY=False, legFontSize=12):
+    # Create figure and axes
+    fig, ax = plt.subplots()
 
-#     # Create figure and axes
-#     fig, ax = plt.subplots()
+    # Plot scatter with error bars
+    if len(xerr)==0: xerr = [0] * len(x) # Sometimes we only use yerr
+    if len(yerr)==0: yerr = [0] * len(y) # Sometimes we only use yerr
 
-#     # Iterate over the hists and plot each one
-#     for i, hist in enumerate(hists):
-#         colour = colours[i]
-#         if not includeBlack: colour = colours[i+1]
-#         counts, bin_edges, _ = ax.hist(hist, bins=nbins, range=(xmin, xmax), histtype='step', edgecolor=colours, linewidth=1.0, fill=False, density=False, color=colour, label=labels[i], log=logY)
+    if len(x) != len(y): print("Warning: x has length", len(x),", while y has length", len(y))
 
-#     # Set x-axis limits
-#     ax.set_xlim(xmin, xmax)
+    ax.errorbar(x, y, xerr=xerr, yerr=yerr, fmt='o', color='black', markersize=4, ecolor='black', capsize=2, elinewidth=1, linestyle='None')
 
-#     ax.set_title(title, fontsize=16, pad=10)
-#     ax.set_xlabel(xlabel, fontsize=14, labelpad=10) 
-#     ax.set_ylabel(ylabel, fontsize=14, labelpad=10) 
+    # Set title, xlabel, and ylabel
+    ax.set_title(title, fontsize=15, pad=10)
+    ax.set_xlabel(xlabel, fontsize=13, labelpad=10) 
+    ax.set_ylabel(ylabel, fontsize=13, labelpad=10) 
 
-#     # Set font size of tick labels on x and y axes
-#     ax.tick_params(axis='x', labelsize=14)  # Set x-axis tick label font size
-#     ax.tick_params(axis='y', labelsize=14)  # Set y-axis tick label font size
-    
-#     if (ax.get_xlim()[1] > 9999 or ax.get_xlim()[1] < 9.999e-3):
-#         ax.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
-#         ax.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
-#         ax.xaxis.offsetText.set_fontsize(14)
-#     if (ax.get_ylim()[1] > 9999 or ax.get_ylim()[1] < 9.999e-3):
-#         ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
-#         ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-#         ax.yaxis.offsetText.set_fontsize(14)
+    # Set font size of tick labels on x and y axes
+    ax.tick_params(axis='x', labelsize=13)  # Set x-axis tick label font size
+    ax.tick_params(axis='y', labelsize=13)  # Set y-axis tick label font size
 
-#     # Add legend to the plot
-#     ax.legend(loc=legPos, frameon=False, fontsize=legFontSize)
+     # Scientific notation
+    if (ax.get_xlim()[1] > 9999 or ax.get_xlim()[1] < 9.999e-3):
+        ax.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+        ax.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+        ax.xaxis.offsetText.set_fontsize(13)
+    if (ax.get_ylim()[1] > 9999 or ax.get_ylim()[1] < 9.999e-3):
+        ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+        ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        ax.yaxis.offsetText.set_fontsize(13)
 
-#     # Save the figure
-#     plt.savefig(fout, dpi=NDPI, bbox_inches="tight")
-#     print("---> Written", fout)
+    # Save the figure
+    plt.savefig(fout, dpi=NDPI, bbox_inches="tight")
+    print("---> Written", fout)
 
-#     # Clear memory
-#     plt.close()
+    # Clear memory
+    plt.close()
+
+def PlotNormEntriesGraph(x, y, xerr=[], yerr=[], title=None, xlabel=None, ylabel=None, fout="scatter.png", NDPI=300):
+
+   # Create a scatter plot with error bars using NumPy arrays 
+
+    # Create figure and axes
+    fig, ax = plt.subplots()
+
+    # Plot scatter with error bars
+    if len(xerr)==0: xerr = [0] * len(x) # Sometimes we only use yerr
+    if len(yerr)==0: yerr = [0] * len(y) # Sometimes we only use yerr
+
+    if len(x) != len(y): print("Warning: x has length", len(x),", while y has length", len(y))
+
+    ax.errorbar(x, y, xerr=xerr, yerr=yerr, fmt='o', color=colours[2], markersize=4, ecolor=colours[2], capsize=2, elinewidth=1, linestyle='None')
+
+    ax.set_ylim(0, 1.1)
+    plt.axhline(y=1, color='gray', linestyle='-', linewidth=1, zorder=0)
+    plt.axhline(y=1.04, color='gray', linestyle='--', linewidth=1, zorder=0)
+    plt.axhline(y=0.96, color='gray', linestyle='--', linewidth=1, zorder=0)
+    # Add label indicating the dashed lines represent +/- 4%
+    # ax.text(0.5, 1.05, r"$\pm 4\%$", horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, fontsize=13)
+
+    # Set title, xlabel, and ylabel
+    ax.set_title(title, fontsize=15, pad=10)
+    ax.set_xlabel(xlabel, fontsize=13, labelpad=10) 
+    ax.set_ylabel(ylabel, fontsize=13, labelpad=10) 
+
+    # Set font size of tick labels on x and y axes
+    ax.tick_params(axis='x', labelsize=13)  # Set x-axis tick label font size
+    ax.tick_params(axis='y', labelsize=13)  # Set y-axis tick label font size
+
+     # Scientific notation
+    if (ax.get_xlim()[1] > 9999 or ax.get_xlim()[1] < 9.999e-3):
+        ax.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+        ax.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+        ax.xaxis.offsetText.set_fontsize(13)
+    if (ax.get_ylim()[1] > 9999 or ax.get_ylim()[1] < 9.999e-3):
+        ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+        ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        ax.yaxis.offsetText.set_fontsize(13)
+
+    # Save the figure
+    plt.savefig(fout, dpi=NDPI, bbox_inches="tight")
+    print("---> Written", fout)
+
+    # Clear memory
+    plt.close()
+
+from matplotlib.colors import LogNorm
+
+def Plot2D(x, y, nBinsX=100, xmin=-1.0, xmax=1.0, nBinsY=100, ymin=-1.0, ymax=1.0, title=None, xlabel=None, ylabel=None, fout="hist.png", cb=True, NDPI=300, logZ=False):
+
+    # Create 2D histogram
+    hist, x_edges, y_edges = np.histogram2d(x, y, bins=[nBinsX, nBinsY], range=[[xmin, xmax], [ymin, ymax]])
+
+    # Set up the plot
+    fig, ax = plt.subplots()
+
+    # Plot the 2D histogram
+    norm = None
+    if logZ: norm=LogNorm()
+    im = ax.imshow(hist.T, cmap='inferno', extent=[xmin, xmax, ymin, ymax], aspect='auto', origin='lower', vmax=np.max(hist), norm=norm) # , norm=cb.LogNorm())
+
+    # Add colourbar
+    if cb: plt.colorbar(im)
+
+    plt.title(title, fontsize=15, pad=10)
+    plt.xlabel(xlabel, fontsize=13, labelpad=10)
+    plt.ylabel(ylabel, fontsize=13, labelpad=10)
+
+     # Scientific notation
+    if (ax.get_xlim()[1] > 9999 or ax.get_xlim()[1] < 9.999e-3):
+        ax.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+        ax.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+        ax.xaxis.offsetText.set_fontsize(13)
+    if (ax.get_ylim()[1] > 9999 or ax.get_ylim()[1] < 9.999e-3):
+        ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+        ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        ax.yaxis.offsetText.set_fontsize(13)
+
+    plt.savefig(fout, dpi=NDPI, bbox_inches="tight")
+    print("---> Written", fout)
+
+    # Clear memory
+
+def Plot2DWith1DProj(x, y, nBinsX=100, xmin=-1.0, xmax=1.0, nBinsY=100, ymin=-1.0, ymax=1.0, title=None, xlabel=None, ylabel=None, fout="hist.png", NDPI=300, logZ=False):
+
+    # Create 2D histogram
+    hist, x_edges, y_edges = np.histogram2d(x, y, bins=[nBinsX, nBinsY], range=[[xmin, xmax], [ymin, ymax]])
+
+    # Set up the plot
+    fig = plt.figure(figsize=(8, 10))
+    gs = fig.add_gridspec(2, 2, width_ratios=[4, 1], wspace=0.1, hspace=0.1)
+
+    # Plot the 2D histogram
+    ax1 = fig.add_subplot(gs[0, 0])
+
+    norm = None
+    if logZ: norm=LogNorm()
+    im = ax1.imshow(hist.T, cmap='inferno', extent=[xmin, xmax, ymin, ymax], aspect='auto', origin='lower', norm=norm)
+
+    # Format main plot axes
+    ax1.set_title(title, fontsize=16, pad=10)
+    ax1.set_xlabel(xlabel, fontsize=14, labelpad=10) 
+    ax1.set_ylabel(ylabel, fontsize=14, labelpad=10) 
+
+    # Set font size of tick labels on x and y axes
+    ax1.tick_params(axis='x', labelsize=14)  # Set x-axis tick label font size
+    ax1.tick_params(axis='y', labelsize=14)  # Set y-axis tick label font size
+
+    # Scientific notation
+    if ax1.get_xlim()[1] > 999:
+        ax1.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+        ax1.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+        ax1.xaxis.offsetText.set_fontsize(14)
+    if ax1.get_ylim()[1] > 999:
+        ax1.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+        ax1.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        ax1.yaxis.offsetText.set_fontsize(14)
+
+    # Draw a horizontal white line at y=50 
+    # ax1.axhline(y=50, color='white', linestyle='--', linewidth=1)
+
+    # Add colourbar
+    ax_cb = fig.add_subplot(gs[0, 1])
+    cbar = plt.colorbar(im, cax=ax_cb)
+    cbar.ax.set_position(cbar.ax.get_position().shrunk(0.25, 1))
+    cbar.ax.set_position(cbar.ax.get_position().translated(0.10, 0))
+    cbar.ax.tick_params(labelsize=14)
+
+    # Project the 2D histogram onto the x-axis
+    hist_x = np.sum(hist, axis=1)
+    bin_centers_x = (x_edges[:-1] + x_edges[1:]) / 2
+
+    # Create a dummy copy of ax1 to be shared with projection axes,
+    # this prevents the original being modified
+    ax1_dummy = ax1.figure.add_axes(ax1.get_position(), frame_on=False)
+    ax1_dummy.set_xticks([])  # Turn off x-axis ticks of ax1_dummy
+    ax1_dummy.set_yticks([])  # Turn off y-axis ticks of ax1_dummy
+    # Make sure that the ranges are the same between ax1 and ax1_dummy
+    ax1_dummy.set_xlim(ax1.get_xlim())
+    ax1_dummy.set_ylim(ax1.get_ylim())
+
+    # Plot the 1D histogram along the x-axis
+    ax2 = fig.add_subplot(gs[1, 0], sharex=ax1_dummy)
+
+    counts, bin_edges, _ = ax2.hist(x, bins=nBinsX, range=(xmin, xmax), histtype='step', edgecolor='black', linewidth=1.0, fill=False)
+
+    # Set the position of ax2 while maintaining the same width and adjusting the height
+    ax2.set_position([ax1.get_position().x0, ax1.get_position().y0 + ax1.get_position().height + 0.01, ax1.get_position().width, ax1.get_position().height / 5])
+
+    # Turn off appropriate tick markers/numbering and spines 
+    ax2.spines['top'].set_visible(False)
+    ax2.spines['right'].set_visible(False)
+    ax2.spines['left'].set_visible(False)
+    ax2.spines['bottom'].set_visible(True)
+
+    ax2.yaxis.set_ticklabels([])
+    # ax2.tick_params(axis='x', which='major', length=ax1.get_tick_params('length'))  # Turn on tick markers
+    ax2.tick_params(axis='y', which='major', length=0)  # Hide tick markers
+
+    # Project the 2D histogram onto the y-axis
+    hist_y = np.sum(hist, axis=0)
+    bin_centers_y = (y_edges[:-1] + y_edges[1:]) / 2
+
+    # Plot the 1D histogram along the y-axis (rotated +90 degrees)
+    ax3 = fig.add_subplot(gs[1, 1], sharey=ax1_dummy)
+    counts, bin_edges, _ = ax3.hist(y, bins=nBinsY, range=(ymin, ymax), histtype='step', edgecolor='black', linewidth=1.0, fill=False, orientation='horizontal')
+
+    # Set the position of ax3 while maintaining the same height and adjusting the width
+    ax3.set_position([ax1.get_position().x0 + ax1.get_position().width + 0.01, ax1.get_position().y0, ax1.get_position().width / 5, ax1.get_position().height])
+
+    # Turn off appropriate tick markers/numbering and spines 
+    ax3.spines['top'].set_visible(False)
+    ax3.spines['right'].set_visible(False)
+    ax3.spines['left'].set_visible(True)
+    ax3.spines['bottom'].set_visible(False)
+
+    ax3.xaxis.set_ticklabels([])
+    ax3.tick_params(axis='x', which='major', length=0)  # Hide tick markers
+    # ax3.tick_params(axis='y', which='major', length=ax1.get_tick_params('length')) 
+
+    # Save figure
+    plt.savefig(fout, dpi=NDPI, bbox_inches="tight")
+    print("---> Written", fout)
+
+    # Clear memory
+    plt.close()
